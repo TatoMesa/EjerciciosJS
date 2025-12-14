f0f7e9e4c043e67eed873f537af1b9fd20b3372b
tatomesa <seba_mesa@hotmail.com>
2025-12-14 18:01:17 -0300

    Ejercicios 1 y 2

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,1 +1,15 @@
-console.log("eje 1");
+
+const boton = document.getElementById("btnColor");
+
+boton.addEventListener("click", () => {
+  const colorAleatorio = generarColor();
+  document.body.style.backgroundColor = colorAleatorio;
+});
+
+function generarColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+}
--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,1 +1,15 @@
-console.log("eje 2");
+let contador = 0;
+
+const botonS = document.getElementById("btnSumar");
+const botonR = document.getElementById("btnRestar");
+const texto = document.getElementById("contador");
+
+botonS.addEventListener("click", () => {
+  contador++;
+  texto.textContent = `Clics: ${contador}`;
+});
+
+botonR.addEventListener("click", () => {
+  contador--;
+  texto.textContent = `Clics: ${contador}`;
+});
